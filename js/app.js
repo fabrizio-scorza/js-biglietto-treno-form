aaf9f44694fa8a6246484bc4ed39ebacb7be0919
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-03-05 14:06:27 +0100

    ADD -  pseudocode milestone1

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// MILESTONE 1:
+// Modifichiamo la logica dell’esercizio di ieri in modo che i dati non arrivino da un prompt ma da un form che l’utente deve compilare.
+// Usando esclusivamente due input: un input per i km e un input per l’età, e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. Cliccando sul bottone recuperate i valori delle input e calcolate il prezzo nello stesso modo che avete usato ieri.
+// La risposta finale (o output) sarà anch’essa da scrivere in console.
+
+//Lettura dei dati dall'input html al click sul bottone
+    //ascolto del click sul bottone
+    //lettura del dato km 
+        //assegnazione del valore di km ad una variabile km
+    //lettura del dato età
+        //asegnazione del valore di età ad una variabile età
+//calcolo del prezzo del biglietto
+    // calcolo del prezzo base del biglietto
+    // applicazione dell'eventuale sconto
+    //stampa del prezzo finale del biglietto
